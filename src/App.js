d6aeb057b34753cccf371bf07406e0cf3e6a12f3
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-07-02 23:57:35 +0530

    Adding some file structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
 
 function App() {
   return (
     <div className="App">
-      <h1>Building an app</h1>
-      {/* <Navigation />
+      <Navigation />
       <Logo />
-      <ImageLinkForm />
+      {/*<ImageLinkForm />
       <FaceRecognition /> */}
     </div>
   );
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Tilt from "react-tilt";
+import "./Logo.css";
+import brain from "./brain.png";
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt"
+        options={{ max: 55 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner pa3">
+          <span>
+            <img style={{ paddingTop: "5px" }} src={brain} alt="logo" />
+          </span>
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
